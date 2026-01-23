a831843afe25119a0580f9fc440bea579af7023d
Roger Reynard <Roger.Reynard@hennepin.us>
2026-01-23 11:36:56 -0700

    - Another debugging test

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -8,12 +8,14 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token = searchParams.get('preview_token')
   const key = searchParams.get('key')
-  const ver = searchParams.get('ver')
+  let ver = searchParams.get('ver')
   const loc = searchParams.get('loc')
 
   if (!ver || !token || !key) {
     return notFound()
   }
+
+  ver = (Number(ver) - 1).toString()
 
   const response = await optimizely.GetContentByKeyAndVersion(
     { key, ver },
